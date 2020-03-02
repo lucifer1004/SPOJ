63cab13586dfd7b00d1db96a1ffcd7142014cd2a
Gabriel Wu <qqbbnease1004@126.com>
2020-03-02 14:57:02 +0800

    update GGD

--- GGD/GGD.py
+++ GGD/GGD.py
@@ -1,6 +1,5 @@
 t = int(input())
 for case_num in range(1, t + 1):
     n = int(input())
-    a = 2 if n == 3 else n // 2
-    b = 3 if n == 3 else n - n % 2
+    a, b = (2, 3) if n == 3 else (n // 2, n - n % 2)
     print('Case {}: {} {}'.format(case_num, a, b))
